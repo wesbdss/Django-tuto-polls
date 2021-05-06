ebf8b716d1e3f996e7d78a63c0c24821432610e8
wesbdss <wesleybenicio4@gmail.com>
2021-05-06 19:42:21 -0300

    issue(#5): Tutorial part 5 Django

--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -3,60 +3,46 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.db.models import F
 
 from .models import Question, Choice
 
-# Create your views here.
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list':latest_question_list,
-    }
-    return render(request, 'polls/index.html', context, )
-
-# é a mesma coisa repetida
-# def detail(request, question_id):
-#     # try:
-#     #     question = Question.objects.get(pk=question_id)
-#     # except Question.DoesNotExist:
-#     #     raise Http404("Question does not exists")
-#     question = get_object_or_404(Question, pk=question_id)
-
-#     return render(request,"polls/detail.html",{'question':question})
-
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request,"polls/results.html",{'question':question})
-
 
 class IndexView(generic.ListView):
+    # Por padrão <app name>/<model name>_list.html seria polls/question_list.html
     template_name = 'polls/index.html'
-    context_object_name = 'lastest_question_list'
+    # Informa o nome da variável a ser passada para view
+    context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         return Question.objects.order_by('-pub_date')[:5]
 
+
 class DetailView(generic.DetailView):
-    model=Question
+    model = Question
+    # Por padrão <app name>/<model name>_detail.html seria polls/question_detail.html
     template_name = 'polls/detail.html'
+
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+# esse não faz, pq é um POST não uma view
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request,'pools/detail.html',
-        {
-            'question': question, 
-            'error_message': "Voce não selecionou uma resposta."}
-        )
+        return render(request, 'polls/detail.html',
+                      {
+                          'question': question,
+                          'error_message': "Voce não selecionou uma resposta."
+                      })
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
-    
-    return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
